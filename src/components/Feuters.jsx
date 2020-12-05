e1d93f590dceacf93efe48f69ac43df921942540
defsmilend <ayxand26@gmail.com>
2020-12-05 16:50:26 +0300

    navbar&route

---
+++ src/components/Feuters.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+
+
+const Feuters = () => {
+    return (
+            <Container fixed>
+                <h1>Перспективы</h1>
+    <p>
+      Мы планируем развиваться и дальше! 
+      Наши планы: 
+      Расширить список тэгов; 
+      увеличить количество направлений обучения; 
+      подключиться к словарю синонимов университета. 
+    </p>
+      <img src="https://cdn.pixabay.com/photo/2016/06/26/23/32/contract-1481586_960_720.png" alt=""></img>
+      </Container>
+    )
+}
+
+export default Feuters
